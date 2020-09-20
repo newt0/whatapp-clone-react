11e4b78a2dcadf2fb7b2372b694e299e9c445eaa
newt0 <py_lambda@yahoo.co.jp>
2020-09-21 00:44:02 +0900

    fix: detach real time listener when finished using it

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,9 +12,13 @@ function Sidebar() {
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
-    db.collection("rooms").onSnapshot((snapshot) =>
+    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
       setRooms(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
     );
+
+    return () => {
+      unsubscribe()
+    }
   }, []);
   return (
     <div className="sidebar">
